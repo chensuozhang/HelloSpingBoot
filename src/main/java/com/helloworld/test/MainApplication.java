7161ece253db422eaba8832d36912b7df66f33cc
BG400343 <wchen343@best-inc.com>
2021-01-11 21:15:35 +0800

    HelloController.java

--- src/main/java/com/helloworld/test/MainApplication.java
+++ src/main/java/com/helloworld/test/MainApplication.java
@@ -1,7 +1,19 @@
 package com.helloworld.test;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * 主程序类
+ *
+ * @SpringBootApplication：这是一个SpringBoot应用，标注springBoot的主配置类
+ */
+@SpringBootApplication
 public class MainApplication {
+
+    public static void main(String[] args) {
+        //springboot应用的启动
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
