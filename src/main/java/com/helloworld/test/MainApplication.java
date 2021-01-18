b239b5a7d46ae3f41c96d3b376e5915c2ea16ac3
BG400343 <wchen343@best-inc.com>
2021-01-18 09:52:42 +0800

    容器、组件注册

--- src/main/java/com/helloworld/test/MainApplication.java
+++ src/main/java/com/helloworld/test/MainApplication.java
@@ -1,13 +1,15 @@
 package com.helloworld.test;
 
 
+import com.helloworld.test.service.HelloService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 /**
  * 主程序类
- *
  * @SpringBootApplication：这是一个SpringBoot应用，标注springBoot的主配置类
  */
 @ImportResource(locations = {"classpath:beans.xml"})
@@ -16,6 +18,18 @@ public class MainApplication {
 
     public static void main(String[] args) {
         //springboot应用的启动
-        SpringApplication.run(MainApplication.class, args);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(MainApplication.class, args);
+
+        /*查看容器中的组件
+        springboot会自动配置好开发常用的场景
+        */
+        String[] names = configurableApplicationContext.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
+//         从容器中获取组件
+//        HelloService h1 = configurableApplicationContext.getBean("HelloService");
+//        HelloService h2 = configurableApplicationContext.getBean("HelloService");
+
     }
 }
--- src/main/java/com/helloworld/test/config/MyAppConfig.java
+++ src/main/java/com/helloworld/test/config/MyAppConfig.java
@@ -7,15 +7,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /*
-指明当前类是一个配置类，替代之前的spring配置文件
+1.指明当前类是一个配置类，替代之前的spring配置文件， 配置类本身也是一个组件
+2.使用@bean注解在类上，给容器中注册组件，且组件是单实例，无论获取多少次，获取的都是同一个组件（默认单实例）
+3.proxyBeanMethods代理bean的方法：
+full：proxyBeanMethods = true  每次启动都要检查是否唯一存在
+lite：proxyBeanMethods = false 每次启动无需进行检查
+4.@Import({})
+5.@Condtional({})
  */
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class MyAppConfig {
 
-    //将方法的返回值添加到容器中；容器中这个组件的默认ID就是此方法名
+    //将方法的返回值添加到容器中；容器中这个组件的默认ID就是此方法名；返回类型即为组件类型，返回的值即为组件在容器中保存的对象
+//    @Bean("自定义的名字")
     @Bean
-    public HelloService helloService02(){
-        System.out.println("给容器中添加组件了...");
+    public HelloService helloService02() {
+        System.out.println("给容器中添加组件了");
         return new HelloService();
     }
 }
